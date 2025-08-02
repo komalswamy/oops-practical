9ee6d52a79185b4af5b7a8cd9f28dcdfcb5e09b7
komalswamy <2420080053@klh.edu.in>
2025-08-02 09:26:26 +0530

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,12 @@
+package prac;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int num = 7;
+        if (num % 2 == 0) {
+            System.out.println("Even number");
+        } else {
+            System.out.println("Odd number");
+        }
+    }
+}
