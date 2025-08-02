57eaba4a46993c71e8559f07b5ceb6753ea93d20
komalswamy <2420080053@klh.edu.in>
2025-08-02 09:25:35 +0530

    Add files via upload

---
+++ Relationaloperators.java
@@ -0,0 +1,10 @@
+package prac;
+
+public class Relationaloperators {
+	    public static void main(String[] args) {
+	        int x = 10, y = 20;
+	        System.out.println(x > y);
+	        System.out.println(x < y);
+	        System.out.println(x == y);
+	    }
+	}
---
+++ TypeCasting.java
@@ -0,0 +1,9 @@
+package prac;
+public class TypeCasting{
+    public static void main(String[] args) {
+        double d = 10.75;
+        int n= (int) d;
+        System.out.println("Double value: " + d);
+        System.out.println("Cast to int: " + n);
+    }
+}
